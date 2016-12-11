526d6b55838ab10f305d44fc0c8a13d5066d86d5
kennyloos <kenneth.loos@siu.edu>
2016-12-11 15:51:54 -0800

    Changed the Level to start at 1 instead of 50.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int floor = 50;
+        int floor = 1;
         World world = new World(floor);
         
         boolean result;
